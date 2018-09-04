d0603a1e4f7ae6149e7eda483434c51a81630c0d
Watcharin Kriengwatana <him10424@gmail.com>
2018-09-04 21:21:29 +0700

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const morgan = require("morgan");
 const app = require("express")();
 const router = require("./routes/index");
+const cors = require("cors");
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`running on port: ${PORT}`);
+    console.log(`running on port: ${PORT}`);
 });
 app.set("json spaces", 2);
+app.use(cors());
 app.use(morgan("dev"));
 app.use("/api", router);
