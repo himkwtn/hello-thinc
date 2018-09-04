2742eee9e563d9e7306ae9ae89a41fcbd9d776e3
Watcharin Kriengwatana <him10424@gmail.com>
2018-09-04 21:29:06 +0700

    vue static file

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const morgan = require("morgan");
 const app = require("express")();
 const router = require("./routes/index");
 const cors = require("cors");
+const serveStatic = require("serve-static");
+const { join } = require("path");
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
@@ -10,4 +13,9 @@ app.listen(PORT, () => {
 app.set("json spaces", 2);
 app.use(cors());
 app.use(morgan("dev"));
+app.use("/", serveStatic(join(__dirname, "/dist")));
 app.use("/api", router);
+
+app.get("*", function(req, res) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
